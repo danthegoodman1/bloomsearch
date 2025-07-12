4c5556fd16dbc11f64d91952f506b8907b626225
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2025-07-12 11:26:31 -0700

    generic file pointer and MaybeFile struct

--- meta_store.go
+++ meta_store.go
@@ -2,10 +2,19 @@ package bloomsearch
 
 import "context"
 
-type MetaStore interface {
+// MetaStore is a generic interface for a metadata store that can be used to store and retrieve file and data block metadata.
+//
+// FilePointer is a pointer to a file in the metadata store, depending on the implementation of the MetaStore and DataStore.
+type MetaStore[FilePointer any] interface {
 	// TODO: define query conditions, return pointers to row groups that the DataStore can read
-	GetRowGroupsForQuery(ctx context.Context, queryConditions map[string]any) ([]*DataBlockMetadata, error)
+	GetRowGroupsForQuery(ctx context.Context, queryConditions map[string]any) ([]MaybeFile[FilePointer], error)
 
 	// WriteFileMetadata writes the file metadata to the store
 	WriteFileMetadata(ctx context.Context, fileMetadata *FileMetadata) error
+}
+
+type MaybeFile[FilePointer any] struct {
+	Pointer    FilePointer
+	Metadata   FileMetadata
+	DataBlocks []DataBlockMetadata
 }
